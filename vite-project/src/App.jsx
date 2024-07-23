b91d02dfcf0827e5b5afe5e09e6b677c05f4a15c
thedarhon <63700542+thedarhon@users.noreply.github.com>
2024-07-23 21:35:59 +0200

    Hero section done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Projects />
+    </>
+  );
+}
+
+export default App;
---
+++ vite-project/src/sections/Projects/Projects.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectsStyles.modules.css';
+
+function Projects() {
+  return (
+    <section id='projects' className={styles.container}>
+      Projects
+    </section>
+  );
+}
+
+export default Projects;
